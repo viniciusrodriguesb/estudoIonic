1a5731c97d863604ac5801fb8ddc81a8a2f048ba
Técnico Informática 2021.1 <51127512021.1@senacrjedu.local>
2022-12-12 10:27:18 -0300

    Aula de mudanca de pagina principal e estilizacao

--- Aula-01/Tutorial/src/app/app-routing.module.ts
+++ Aula-01/Tutorial/src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
   {
     path: 'navegacao',
     loadChildren: () => import('./pages/navegacao/navegacao.module').then( m => m.NavegacaoPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   }
 ];
 
